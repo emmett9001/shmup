ce47d1360278944d2eebed3583b9276438e8d1c5
emmett9001 <emmett.butler321@gmail.com>
2014-04-20 20:04:43 -0400

    add cyclicboundedstraightlinemover header

---
+++ CyclicBoundedStraightLineMover.h
@@ -0,0 +1,24 @@
+#ifndef CYCLICBOUNDEDSTRAIGHTLINEMOVER_H
+#define CYCLICBOUNDEDSTRAIGHTLINEMOVER_H
+
+#include "StraightLineMover.h"
+#include "ofMain.h"
+
+class BulletPattern;
+
+class CyclicBoundedStraightLineMover : public StraightLineMover
+{
+    public:
+        static const string SLUG;
+        float wavelength;
+        CyclicBoundedStraightLineMover();
+        CyclicBoundedStraightLineMover(ofVec2f direction, float wavelength);
+        CyclicBoundedStraightLineMover(BulletPattern *attached, ofVec2f direction, float wavelength);
+
+        void move(float deltatime);
+        string describe();
+    protected:
+    private:
+};
+
+#endif // CYCLICBOUNDEDSTRAIGHTLINEMOVER_H
